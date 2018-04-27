9992840ffd725ebce98d0417c9de16357308826d
Matias González <matialem8@gmail.com>
2018-04-26 23:24:44 -0300

    cambios en el login

--- js/funcionesLogin.js
+++ js/funcionesLogin.js
@@ -1,4 +1,5 @@
-window.fbAsyncInit = function() {
+window.fbAsyncInit = function() 
+{
     FB.init({
       appId            : '1071078366372805',
       autoLogAppEvents : true,
@@ -7,16 +8,19 @@ window.fbAsyncInit = function() {
     });
 }; 
 
-(function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "https://connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
+(function(d, s, id)
+{
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-$(function() {
-  $("#botonFbLogin").click(function() {
+$(function() 
+{
+  $("#botonFbLogin").click(function() 
+  {
     FB.login(function(response )
     {
       if (response.status === 'connected' )
@@ -36,7 +40,8 @@ $(function() {
   });
 });
 
-function checkLoginState() {
+function checkLoginState() 
+{
   FB.getLoginStatus(function(response) 
   {
     statusChangeCallback(response);
@@ -45,12 +50,6 @@ function checkLoginState() {
 
 function statusChangeCallback(response) 
 {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
     if (response.status === 'connected') 
     {
       // Logged into your app and Facebook.
@@ -58,19 +57,16 @@ function statusChangeCallback(response)
     } else 
     {
       // The person is not logged into your app or we are unable to tell.
-      document.getElementById('panelLogin').innerHTML = 'Please log ' +
-        'into this app.';
+      document.getElementById('panelLogin').innerHTML = 'Please log into this app.';
     }
 }
 
 function testAPI() 
 {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('panelLogin').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
+  FB.api('/me', function(response) 
+  {
+    document.getElementById('panelLogin').innerHTML = 'Thanks for logging in, ' + response.name + '!';
+  });
 }
 
 $(function() {
