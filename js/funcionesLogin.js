c6da8c90a21f3d9d7bbfe83d5d6d064669ac211d
Matias González <matialem8@gmail.com>
2018-04-26 22:42:47 -0300

    no message

--- js/funcionesLogin.js
+++ js/funcionesLogin.js
@@ -1,37 +1,34 @@
 window.onload = function() 
 {
-  cargarAPIFacebook();
 	cargarTema();
+  FB.Event.subscribe('xfbml.render', finished_rendering);
 }
 
-function cargarAPIFacebook()
-{
-  window.fbAsyncInit = function() {
-      FB.init({
-        appId            : '1071078366372805',
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v2.12'
-      });
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '1071078366372805',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.12'
+    });
 
-      FB.getLoginStatus(function(response){
-        if (response.status === 'connected' )
+    FB.getLoginStatus(function(response){
+      if (response.status === 'connected' )
+      {
+        document.getElementById("panelLogin").innerHTML = 'We are connected';
+      }
+      else 
+        if (response.status === 'not_authorized')
         {
-          document.getElementById("panelLogin").innerHTML = 'We are connected';
+         document.getElementById("panelLogin").innerHTML = 'We are not logged in';
         }
-        else 
-          if (response.status === 'not_authorized')
-          {
-           document.getElementById("panelLogin").innerHTML = 'We are not logged in';
-          }
-          else
-          {
-            document.getElementById("panelLogin").innerHTML = 'You are not logged into Facebook';
-          }
+        else
+        {
+          document.getElementById("panelLogin").innerHTML = 'You are not logged into Facebook';
+        }
 
-      });
-  };  
-}
+    });
+}; 
 
 (function(d, s, id){
          var js, fjs = d.getElementsByTagName(s)[0];
@@ -88,3 +85,48 @@ $(function() {
     window.location.href = "index.html";
   });
 });
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) 
+  {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) 
+{
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') 
+    {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else 
+    {
+      // The person is not logged into your app or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+}
+
+function testAPI() 
+{
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+}
+
+var finished_rendering = function() {
+  console.log("finished rendering plugins");
+  var spinner = document.getElementById("spinner");
+  spinner.removeAttribute("style");
+  spinner.removeChild(spinner.childNodes[0]);
+}
+
