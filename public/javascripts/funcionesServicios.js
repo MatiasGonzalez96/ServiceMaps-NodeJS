5f70372adf713ab6ab2f26a20c88764875017882
Matias Gonzalez <matialem8@gmail.com>
2018-05-08 20:13:34 -0300

    Mejora de los servicios

--- public/javascripts/funcionesServicios.js
+++ public/javascripts/funcionesServicios.js
@@ -1,8 +1,10 @@
-function initMap() 
+var posActual;
+
+function initMap()
 {
   var ubicacionServicio = new google.maps.LatLng(parseFloat(document.getElementById('map').getAttribute("latitud")), parseFloat(document.getElementById('map').getAttribute("longitud")));
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 16,
+    zoom: 15,
     center: ubicacionServicio,
     styles: [
       {
@@ -14,10 +16,38 @@ function initMap()
       }
     ]
   });
-  var marker = new google.maps.Marker({
+
+  if (navigator.geolocation)
+  {
+        navigator.geolocation.getCurrentPosition(function(position)
+        {
+              posActual =
+              {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              };
+
+              mostrarDistanciaServicio();
+
+              markerA = new google.maps.Marker({
+                  position: posActual,
+                  title: "Mi Ubicación",
+                  map: map
+              });
+
+              directionsService = new google.maps.DirectionsService,
+              directionsDisplay = new google.maps.DirectionsRenderer({
+                map: map
+              }),
+
+              // get route from A to B
+              calculateAndDisplayRoute(directionsService, directionsDisplay, posActual, ubicacionServicio);
+       });
+  }
+
+  var markerB = new google.maps.Marker({
     position: ubicacionServicio,
     map: map,
-    animation: google.maps.Animation.DROP
   });
 }
 
@@ -32,3 +62,34 @@ $(function() {
     window.location.href = "/";
   });
 });
+
+function mostrarDistanciaServicio()
+{
+  var latitud = parseFloat(document.getElementById('map').getAttribute("latitud"));
+  var longitud = parseFloat(document.getElementById('map').getAttribute("longitud"));
+  var destino = new google.maps.LatLng(latitud, longitud);
+  var distancia = Math.round(getDistance(destino));
+  document.getElementById("distanciaServicio").innerHTML = "¡Usted se encuentra a " + distancia + " metros del servicio!";
+}
+
+function getDistance(destino)
+ {
+     var origen = new google.maps.LatLng(posActual.lat, posActual.lng);
+     var distance = google.maps.geometry.spherical.computeDistanceBetween(origen, destino);
+     return distance;
+ }
+
+ function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB)
+ {
+  directionsService.route({
+    origin: pointA,
+    destination: pointB,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
