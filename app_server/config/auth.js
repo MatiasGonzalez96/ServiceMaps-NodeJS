8b373193d482dff6fb79b30efc7911506a5b5447
Matias Gonzalez <matialem8@gmail.com>
2018-06-10 18:30:45 -0300

    Cambios login

--- app_server/config/auth.js
+++ app_server/config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '1071078366372805',
 		'clientSecret': '9a416635622646a3d42f8124302ed008',
-		'callbackURL': '/auth/facebook/callback'
+		'callbackURL': '/'
 	}
 }
