fcc2ae00de9ee03edf12d8a49884c28c84edec34
Matias Gonzalez <matialem8@gmail.com>
2018-06-10 18:57:54 -0300

    cambios

--- app_server/routes/auth.js
+++ app_server/routes/auth.js
@@ -4,7 +4,10 @@ var passport = require('passport');
 
 const auth = require('../controllers/auth');
 
-router.get('/auth/facebook', auth.facebook);
-router.get('/auth/facebook/callback', auth.facebookAuth, auth.facebookCallback);
+
+router.get('/facebook', auth.facebook);
+router.get('/facebook/callback', auth.facebookAuth, auth.facebookCallback);
+
+router.get('/logout', auth.logout);
 
 module.exports = router;
