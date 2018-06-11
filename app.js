6b472c2d51ed6c04f94d57333cde366e4368bf14
Matias Gonzalez <matialem8@gmail.com>
2018-06-10 23:32:42 -0300

    Cambios

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
 app.use('/api', apiRouter);
-app.use('/auth', authRouter);
+//app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
