76db09c38fb6fda93d565a824df4c9c609752745
Matias Gonzalez <matialem8@gmail.com>
2018-06-11 18:36:09 -0300

    cambios

--- app_server/config/passport-setup.js
+++ app_server/config/passport-setup.js
@@ -15,7 +15,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new FacebookStrategy({
     clientID: '1071078366372805',
     clientSecret: '9a416635622646a3d42f8124302ed008',
-    callbackURL: 'https://servicemaps.herokuapp.com/auth/facebook/callback'
+    callbackURL: 'https://servicemaps.herokuapp.com/auth/facebook/callback',
+    profileFields: ['id', 'displayName','emails']
   },
   function(accessToken, refreshToken, profile, done) {
     	process.nextTick(function(){
@@ -25,17 +26,16 @@ passport.use(new FacebookStrategy({
     			if(user)
     				return done(null, user);
     			else {
-    				var newUser = new User();
-    				newUser.id = profile.id;
-    				newUser.name = profile.displayName;
-    				newUser.email = profile.emails[0].value;
+    				newUser = new User({
+                        id: profile.id,
+                        email: profile.emails[0].value,
+                        name: profile.displayName
+                    });
 
-    				newUser.save(function(err){
-    					if(err)
-    						throw err;
-    					return done(null, newUser);
-    				})
-    				console.log(profile);
+                    newUser.save(function(err, newUser) {
+                        if(err) return done(err);
+                        done(null,newUser);
+                    });
     			}
     		});
     	});
